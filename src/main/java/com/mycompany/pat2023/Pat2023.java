da3a1f35367ee0cc71f4528fb2081dae5785a92d
sawlihac <csawliha@gmail.com>
2023-06-17 14:14:53 +0200

    My first commit

---
+++ src/main/java/com/mycompany/pat2023/Pat2023.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pat2023;
+
+/**
+ *
+ * @author csawl
+ */
+public class Pat2023 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
