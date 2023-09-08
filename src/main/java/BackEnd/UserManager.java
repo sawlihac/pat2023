ed26ef06028feddd91cf642a4488986183d5a105
sawlihac <csawliha@gmail.com>
2023-09-08 08:36:09 +0200

    Pat 2023 Ui and login code done

---
+++ src/main/java/BackEnd/UserManager.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package BackEnd;
+
+import com.sun.source.tree.ImportTree;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author csawl
+ */
+public class UserManager {
+
+    public static boolean validateLogin(String usernameTF, String passwordTF) {
+        try {
+            String line = "";
+            
+            File F = new File("Data\\Practice.txt");
+            Scanner filesc = new Scanner(F);
+            while (filesc.hasNextLine()) {
+                line = filesc.nextLine();
+                Scanner linesc = new Scanner(line).useDelimiter("#");
+
+                String currentUsername = linesc.next();
+                String currentPassword = linesc.next();
+
+                if (currentUsername.equals(usernameTF)&&currentPassword.equals(passwordTF)) {
+                    return true;
+                }
+            }
+            
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(UserManager.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return false;
+    }
+
+}
